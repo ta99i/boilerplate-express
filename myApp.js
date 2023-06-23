b1ce61e7764cd8e15079ac47cbdd65b9234eda9f
Taki <abdesselamtaki@icloud.com>
2023-06-23 21:32:21 +0200

    modified:   myApp.js

--- myApp.js
+++ myApp.js
@@ -2,7 +2,7 @@ let express = require("express");
 let app = express();
 console.log("Hello World");
 app.get("/", (req, res) => {
-  const path = __dirname + "./views/index.html";
-  res.send(path);
+  const path = __dirname + "/views/index.html";
+  res.sendFile(path);
 });
 module.exports = app;
