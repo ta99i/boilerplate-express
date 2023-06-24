23cde82476e3dde8e2b7be529078d1932d4be0a6
Taki <abdesselamtaki@icloud.com>
2023-06-24 17:04:42 +0200

    modified:   myApp.js

--- myApp.js
+++ myApp.js
@@ -1,8 +1,38 @@
 let express = require("express");
+let bodyParser = require("body-parser");
 let app = express();
-console.log("Hello World");
+app.use("/public", express.static(__dirname + "/public"));
+app.use((req, res, next) => {
+  console.log(req.method + " " + req.path + " - " + req.ip);
+  next();
+});
+app.use(bodyParser.urlencoded({ extended: false }));
 app.get("/", (req, res) => {
-  const path = __dirname + "/views/index.html";
-  res.sendFile(path);
+  res.sendFile(__dirname + "/views/index.html");
+});
+app.get("/json", (req, res) => {
+  if (process.env["MESSAGE_STYLE"] == "uppercase") {
+    response = { message: "HELLO JSON" };
+  } else {
+    response = { message: "Hello json" };
+  }
+  res.json(response);
+});
+app.get(
+  "/now",
+  (req, res, next) => {
+    const ttime = new Date().toString();
+    req.time = { time: ttime };
+    next();
+  },
+  (req, res) => {
+    res.send(req.time);
+  }
+);
+app.get("/:word/echo", (req, res) => {
+  res.json({ echo: req.params.word });
+});
+app.post("/name", (req, res) => {
+  res.json({ name: req.body.first + " " + req.body.last });
 });
 module.exports = app;
